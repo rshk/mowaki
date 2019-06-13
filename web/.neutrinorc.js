8cbe22e4259d2960605adeaca47ef91102e6602e
Samuele Santi <sam@reinventsoftware.io>
2019-06-13 15:31:06 +0100

    Move browsers configuration to .browserslistrc
    browsers option to Autoprefixer is deprecated

--- web/.neutrinorc.js
+++ web/.neutrinorc.js
@@ -32,12 +32,6 @@ module.exports = {
                     options: {
                         plugins: [
                             require('autoprefixer')({
-                                browsers: [
-                                    '>1%',
-                                    'last 4 versions',
-                                    'Firefox ESR',
-                                    'not ie < 9', // React doesn't support IE8 anyway
-                                ],
                                 flexbox: 'no-2009',
                             }),
                         ]
