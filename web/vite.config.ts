c86a50d1ea074c23078d7e12a682fc9364237f2e
Samuele Santi <samuele@samuelesanti.com>
2026-05-18 12:09:59 +0200

    custom web port

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [react()],
+        server: {
+            port: env.APP_PORT ? Number(env.APP_PORT) : 5173,
+            /* port: 8888, */
+        },
+    };
+});
