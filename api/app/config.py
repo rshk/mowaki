86c663a8f75ba2ca9175513309d5b0caeb93fad3
Samuele Santi <samuele@samuelesanti.com>
2026-06-06 12:40:13 +0200

    wip

--- api/app/config.py
+++ api/app/config.py
@@ -3,27 +3,30 @@ from __future__ import annotations
 from typing import Annotated
 
 from pydantic import (
-    BaseModel,
+    BeforeValidator,
     Field,
     HttpUrl,
+    PlainSerializer,
+    PlainValidator,
     PostgresDsn,
     # RedisDsn,
 )
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, NoDecode
 
 
 class Config(BaseSettings):
+    # Used to generate direct links to the application.
+    frontend_url: HttpUrl
     database_url: PostgresDsn
     # redis_url: RedisDsn
-    smtp_url: str = "dummy://"
-    frontend_url: HttpUrl = Field()
-    development_mode: bool = False
-    dev: Annotated[DevConfig, Field(default_factory=lambda: DevConfig())]
-
-
-class DevConfig(BaseModel):
-    enable_unauthenticated_login: bool = False
-    enable_custom_login_url: bool = False
+    smtp_url: Annotated[str, Field(default="dummy://")]
+    cors_origins: Annotated[
+        list[str],
+        Field(default_factory=list),
+        NoDecode,
+        BeforeValidator(lambda x: x.split()),
+        PlainSerializer(lambda x: " ".join(x), return_type=str),
+    ]
 
 
 def get_config_from_env() -> Config:
--- api/tests/unit/test_config.py
+++ api/tests/unit/test_config.py
@@ -2,8 +2,22 @@
 Test configuration loading machinery
 """
 
+from pydantic import HttpUrl, PostgresDsn
 import pytest
+from app.config import get_config_from_env
+
 
 def test_load_minimal_config(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setenv("DATABASE_URL", "postgres://user:pass@database/dbname")
+    monkeypatch.delenv("SMTP_URL", raising=False)
+    monkeypatch.setenv("FRONTEND_URL", "https://www.example.com")
+    monkeypatch.setenv("CORS_ORIGINS", "https://www.example.com https://www2.example.com")
 
-    pass
+    config = get_config_from_env()
+    assert config.database_url == PostgresDsn("postgres://user:pass@database/dbname")
+    assert config.smtp_url == "dummy://"
+    assert config.frontend_url == HttpUrl("https://www.example.com")
+    assert config.cors_origins == [
+        "https://www.example.com",
+        "https://www2.example.com",
+    ]
