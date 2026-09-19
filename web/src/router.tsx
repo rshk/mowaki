3431bd7ff90b655c1f7539d0a216497d3ad58e1f
Samuele Santi <samuele@samuelesanti.com>
2026-09-19 16:40:49 +0200

    tabs -> spaces

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,27 +1,30 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
-import { getContext } from './integrations/tanstack-query/root-provider'
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+import { getContext } from "./integrations/tanstack-query/root-provider";
 
 export function getRouter() {
-  const context = getContext()
+    const context = getContext();
 
-  const router = createTanStackRouter({
-    routeTree,
-    context,
-    scrollRestoration: true,
-    defaultPreload: 'intent',
-    defaultPreloadStaleTime: 0,
-  })
+    const router = createTanStackRouter({
+        routeTree,
+        context,
+        scrollRestoration: true,
+        defaultPreload: "intent",
+        defaultPreloadStaleTime: 0,
+    });
 
-  setupRouterSsrQueryIntegration({ router, queryClient: context.queryClient })
+    setupRouterSsrQueryIntegration({
+        router,
+        queryClient: context.queryClient,
+    });
 
-  return router
+    return router;
 }
 
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: ReturnType<typeof getRouter>
-  }
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: ReturnType<typeof getRouter>;
+    }
 }
